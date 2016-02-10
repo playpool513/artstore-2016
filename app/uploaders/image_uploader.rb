6c513a1a38130ad9143f2b8017232da05b9484ff
playpool513 <playpool513@gmail.com>
2016-02-10 21:59:54 +0800

    uploader settings

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -23,6 +23,16 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
+
+  process resize_to_fit: [800, 800]
+
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
+
+  version :medium do
+    process resize_to_fill: [400,400]
+  end
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]

487df8e4ce4b33f57892b612295850ed9bece1be
playpool513 <playpool513@gmail.com>
2016-02-13 23:44:50 +0800

    show count of cart.cart_items

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_cart
+
   def admin_required
     redirect_to "/" if !current_user.admin?
   end

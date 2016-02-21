3849ee1d161b2d734f4dfc93c0b2d304dc6b4448
playpool513 <playpool513@gmail.com>
2016-02-21 15:01:38 +0800

    w2 hw6 cart_item quantity can not be larger than product quantity

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,7 +4,15 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @item = @cart.cart_items.find_by(product_id: params[:id])
-    @item.update(item_params)
+    product_quantity = @item.product.quantity
+
+    if product_quantity >= item_params[:quantity].to_i
+      @item.update(item_params)
+      flash[:notice] = "成功變更數量"
+    else
+      flash[:warning] = "商品可售數量只剩 #{product_quantity}，不足以加入購物車。"
+    end
+
     redirect_to carts_path
   end
 
